95ad228f97fddeb34675d521b66b74af6e848fab
francoisbirgand <birgand@ncsu.edu>
2019-03-21 12:23:48 -0400

    Lab10 commits

--- extra.R
+++ extra.R
@@ -4,3 +4,9 @@ cumQSort<-cumQSort/tail(cumQSort,1)*100
 cumLSort<-c(0,(cumsum(LSort[-1])+cumsum(head(LSort,-1)))/2)
 cumLSort<-cumLSort/tail(cumLSort,1)*100
 ```
+
+```{r}
+HGstartdate<-as.POSIXct("1997-10-24 21:00")
+HGenddate<-as.POSIXct("1997-10-25 18:00")
+HG<-subset(Q, D>=HGstartdate & D<=HGenddate)
+```
